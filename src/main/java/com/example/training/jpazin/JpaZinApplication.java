00d574093403f2c9bc43716f7166305b293753e9
LiiPeee <luiznuubb@gmail.com>
2022-03-13 13:36:44 -0300

    Project created

---
+++ src/main/java/com/example/training/jpazin/JpaZinApplication.java
@@ -0,0 +1,13 @@
+package com.example.training.jpazin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaZinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaZinApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/training/jpazin/JpaZinApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.training.jpazin;
+
+
+//import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaZinApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
